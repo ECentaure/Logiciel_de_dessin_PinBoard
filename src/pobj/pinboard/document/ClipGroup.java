597e2513a660819cf4f494085c2841cd8ca606a5
Centaure Emeline <Centaure Emeline@LAPTOP-HKPVS8OT>
2020-04-18 20:13:13 +0200

    derniere correction ClipGroup

--- src/pobj/pinboard/document/ClipGroup.java
+++ src/pobj/pinboard/document/ClipGroup.java
@@ -7,7 +7,8 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class ClipGroup extends AbstractClip implements Composite{
-	private List<Clip> liste_clip;
+	
+	private List<Clip> liste_clip = new ArrayList<Clip>();
 	private double left;
 	private double top;
 	private double right;
@@ -16,12 +17,12 @@ public class ClipGroup extends AbstractClip implements Composite{
 	
 	public ClipGroup() {
 		
-		super(0,0,0,0, Color.ALICEBLUE);
-		liste_clip = new ArrayList<Clip>(); //!!!!!!!!!!!
-		left = 0;
-		top = 0;
-		right = 0;
-		bottom = 0 ;
+		super(0.0,0.0,0.0,0.0, Color.ALICEBLUE);
+		
+		left = 0.0;
+		top = 0.0;
+		right =0.0;
+		bottom = 0.0 ;
 		
 	}
 
@@ -38,18 +39,56 @@ public class ClipGroup extends AbstractClip implements Composite{
 
 	@Override
 	public void setGeometry(double left, double top, double right, double bottom) {
-		move(right - left , bottom - top );
+		
+		move(0, 0 );
 		
 	}
 
 	@Override
 	public void move(double x, double y) {
-		for(Clip e : liste_clip) {
-			e.move(x, y);	
+		
+		for(Clip elem : liste_clip) {
+			elem.move(x, y);	
 		}
+		maj_coord();
+		
 		
 	}
 
+	public void maj_coord() {
+		if(!liste_clip.isEmpty()) {
+			
+			double maxR = liste_clip.get(0).getRight();
+			double maxL = liste_clip.get(0).getLeft();
+			double maxT = liste_clip.get(0).getTop();
+			double maxB = liste_clip.get(0).getBottom();
+			
+			for(Clip elem : liste_clip) {
+				
+				if(elem.getRight() > maxR) 
+				{
+					maxR = elem.getRight();
+					
+				} if(elem.getLeft() < maxL) 
+				{
+					maxL = elem.getLeft();
+							
+				}
+				 if(elem.getTop() < maxT) 
+				{
+					maxT = elem.getTop();
+				}
+				 if(elem.getBottom() > maxB) 
+				{
+					maxB = elem.getBottom();
+				}
+						
+			}	
+		
+			super.setGeometry(maxL, maxT, maxR, maxB);
+		}
+		 
+	}
 
 	@Override
 	public Clip copy() {
@@ -68,39 +107,20 @@ public class ClipGroup extends AbstractClip implements Composite{
 
 	@Override
 	public void addClip(Clip toAdd) {
-		liste_clip.add(toAdd);
+		this.liste_clip.add(toAdd);
+		//System.out.println(liste_clip.size());
 		maj_coord();
+		
 	}
 
 	@Override
 	public void removeClip(Clip toRemove) {
+		
 		liste_clip.remove(toRemove);
-		maj_coord();
+		this.maj_coord();
 		
 	}
 	
-	public void maj_coord() {
-		for(Clip e : liste_clip) {
-			
-			if(e.getRight() > right) 
-			{
-				right = e.getRight();
-			}else if(e.getLeft() > left) 
-			{
-				left = e.getLeft();
-			}
-			else if(e.getTop() > top) 
-			{
-				top = e.getTop();
-			}
-			else if(e.getBottom() > bottom) 
-			{
-				bottom = e.getBottom();
-			}
-			
-		}
-		
-		super.setGeometry(left, top, right, bottom);
-	}
+
 
 }
